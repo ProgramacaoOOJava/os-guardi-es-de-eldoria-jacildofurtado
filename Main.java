5c4d3eb50fdc556d3d215952a233d2d8456d877c
Jacildo <jacildofurtado@gmail.com>
2026-06-15 15:57:53 -0300

    Desafio Nível Novato
    Adicionado a classe Personagem com um construtor e um método para a exibição dos status dos personagens.

--- Main.java
+++ Main.java
@@ -1,10 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        // Instanciação de personagens
         Personagem heroi1 = new Personagem("Arthus", "Guerreiro", 1, 100, 10.5);
+        Personagem heroi2 = new Personagem("Luna", "Mago", 1, 80, 12.0);
 
-        .
-        .
-        .
-
+        // Chamada do método para exibir o status dos personagens
+        heroi1.exibirStatus();
+        heroi2.exibirStatus();
     }
 }
--- Personagem.java
+++ Personagem.java
@@ -1,23 +1,29 @@
 public class Personagem {
+    // Atributos do personagem
     String nome;
     String classe;
     int nivel;
     int pontosDeVida;
     double poderBase;
 
+    // Construtor para inicializar os atributos do personagem
     public Personagem(String nome, String classe, int nivel, int pontosDeVida, double poderBase) {
         this.nome = nome;
-        .
-        .
-        .
+        this.classe = classe;
+        this.nivel = nivel;
+        this.pontosDeVida = pontosDeVida;
+        this.poderBase = poderBase;
 
     }
 
+    // Método para exibir o status do personagem
     public void exibirStatus() {
         System.out.println("--- Status do Personagem ---");
-        .
-        .
-        .
+        System.out.println("Nome: " + nome);
+        System.out.println("Classe: " + classe);
+        System.out.println("Nível: " + nivel);
+        System.out.println("Pontos de Vida: " + pontosDeVida);
+        System.out.println("Poder Base: " + poderBase);
 
     }
 }
